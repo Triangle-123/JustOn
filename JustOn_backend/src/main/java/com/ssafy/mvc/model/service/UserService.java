8ecdf7279db65d2e9e5df53b4331cd909cbc3936
JPhong <hsc3757@gmail.com>
2024-11-13 22:45:40 +0900

    1113-committ

--- JustOn_backend/src/main/java/com/ssafy/mvc/model/service/UserService.java
+++ JustOn_backend/src/main/java/com/ssafy/mvc/model/service/UserService.java
@@ -1,11 +1,9 @@
 package com.ssafy.mvc.model.service;
 
-import org.springframework.stereotype.Service;
-
 import com.ssafy.mvc.model.dto.User;
 
-@Service
 public interface UserService {
+
 	User login(String id, String password);
 	
 	boolean signUp(User user);
