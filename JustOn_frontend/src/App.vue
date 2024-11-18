e2eec59ab0b64aa220260038322c9d8f88e29706
Jaidy <hyjgood2000@naver.com>
2024-11-18 21:54:53 +0900

    dev -> yeon

--- JustOn_frontend/src/App.vue
+++ JustOn_frontend/src/App.vue
@@ -1,31 +1,18 @@
 <template>
   <SideHeader />
-  <RouterView/>
+  <RouterView />
   <!-- <DiaryList /> -->
   <!-- <VideoTest /> -->
   <!-- <RouterLink :to="{name:home}">home</RouterLink> -->
 
   <!-- <LoginTest /> -->
-
 </template>
 
 <script setup>
-
 import SideHeader from "@/components/SideHeader.vue";
 // import DiaryList from "./components/DiaryList.vue";
 // import VideoTest from "@/components/VideoTest.vue";
 // import Home from "./components/Home.vue";
-import { useUserStore } from "@/stores/user";
-import {ref} from 'vue';
-const init = ref(false);
-const userStore = useUserStore();
-
-const userInfo = async () => {
-  await userStore.getUser();
-  init.value = true;
-}
-userInfo();
-
 </script>
 
 <style></style>
