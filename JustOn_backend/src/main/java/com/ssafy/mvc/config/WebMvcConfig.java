eebdb8494ac899e63669827cbd75138bcb36d175
Jaidy <hyjgood2000@naver.com>
2024-11-27 03:07:07 +0900

    241127 last

--- JustOn_backend/src/main/java/com/ssafy/mvc/config/WebMvcConfig.java
+++ JustOn_backend/src/main/java/com/ssafy/mvc/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.ssafy.mvc.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -32,4 +33,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	            .addResourceLocations("file:///" + uploadDir);
 	    
 	}
+	
+	@Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/*")
+        .allowedOrigins("")
+        .allowedMethods("*");
+
+    }
 }
--- JustOn_frontend/src/App.vue
+++ JustOn_frontend/src/App.vue
@@ -1,36 +1,35 @@
-<template >
-    <SideHeader />
-    <RouterView />
-    <!-- <MetronomeBtn v-if="userStore.user"/> -->
-    <!-- <ExSuggest v-if="userStore.user"/> -->
-    <!-- <UserCustom v-if="userStore.user"/> -->
+<template>
+  <SideHeader />
+  <RouterView />
+  <MetronomeBtn v-if="userStore.user" />
+  <ExSuggest v-if="userStore.user" />
+  <UserCustom v-if="userStore.user" />
 
+  <!-- <DiaryList /> -->
+  <!-- <VideoTest /> -->
+  <!-- <RouterLink :to="{name:home}">home</RouterLink> -->
 
-    <!-- <DiaryList /> -->
-    <!-- <VideoTest /> -->
-    <!-- <RouterLink :to="{name:home}">home</RouterLink> -->
-    
-    <!-- <LoginTest /> -->
-    <!-- <MainLoginedView /> -->
-    
-    <div
-        v-for="(circle, index) in circles"
-        :key="index"
-        class="circle"
-        :id="'circle'+index"
-        :style="{
-          width: circle.size + 'px',
-          height: circle.size + 'px',
-          transform: `translate(${circle.x}px, ${circle.y}px)`
-        }"
-      ></div>
+  <!-- <LoginTest /> -->
+  <!-- <MainLoginedView /> -->
+
+  <div
+    v-for="(circle, index) in circles"
+    :key="index"
+    class="circle"
+    :id="'circle' + index"
+    :style="{
+      width: circle.size + 'px',
+      height: circle.size + 'px',
+      transform: `translate(${circle.x}px, ${circle.y}px)`,
+    }"
+  ></div>
 </template>
 
 <script setup>
 import { onMounted, watch, ref, onUpdated } from "vue";
 import SideHeader from "@/components/SideHeader.vue";
 import MetronomeBtn from "./components/MetronomeBtn.vue";
-import ExSuggest from "@/components/ExSuggest.vue"; 
+import ExSuggest from "@/components/ExSuggest.vue";
 import { useUserStore } from "./stores/user";
 import UserCustom from "./components/userCustom.vue";
 const userStore = useUserStore();
@@ -39,19 +38,34 @@ const userStore = useUserStore();
 // import Home from "./components/Home.vue";
 // import MainLoginedView from "@/components/views/MainLoginedView.vue";
 
-watch(() => userStore.user,() => {
-  if(userStore.user.color) {
-    console.log('user', userStore.user);
-    const color1 = userStore.user.color.split(".")[0];
-    const color2 = userStore.user.color.split(".")[1];
-    const color3 = userStore.user.color.split(".")[2];
-    
-    document.documentElement.style.setProperty('--bg-gradient-color1', color1);
-    document.documentElement.style.setProperty('--bg-gradient-color2', color2);
-    document.documentElement.style.setProperty('--bg-gradient-color3', color3);
-    document.documentElement.style.setProperty('--juston-gradient-1', `linear-gradient(to right, ${color1}, ${color2}, ${color3})`);
+watch(
+  () => userStore.user,
+  () => {
+    if (userStore.user.color) {
+      console.log("user", userStore.user);
+      const color1 = userStore.user.color.split(".")[0];
+      const color2 = userStore.user.color.split(".")[1];
+      const color3 = userStore.user.color.split(".")[2];
+
+      document.documentElement.style.setProperty(
+        "--bg-gradient-color1",
+        color1
+      );
+      document.documentElement.style.setProperty(
+        "--bg-gradient-color2",
+        color2
+      );
+      document.documentElement.style.setProperty(
+        "--bg-gradient-color3",
+        color3
+      );
+      document.documentElement.style.setProperty(
+        "--juston-gradient-1",
+        `linear-gradient(to right, ${color1}, ${color2}, ${color3})`
+      );
+    }
   }
-})
+);
 
 // 스크롤바를 고려한 화면의 크기 제한
 const screenWidth = document.documentElement.clientWidth;
@@ -60,63 +74,73 @@ const screenHeight = document.documentElement.clientHeight;
 const isAnimated = ref(false);
 // 원의 상태를 관리하는 reactive 데이터
 const circles = ref([
-      {
-        size: 500,
-        x: Math.random() * screenWidth,
-        y: Math.random() * screenHeight,
-        speedX: (Math.random() - 0.5) * 4,
-        speedY: (Math.random() - 0.5) * 4,
-      },
-      {
-        size: 500,
-        x: Math.random() * screenWidth,
-        y: Math.random() * screenHeight,
-        speedX: (Math.random() - 0.5) * 4,
-        speedY: (Math.random() - 0.5) * 4,
-      },
-      {
-        size: 500,
-        x: Math.random() * screenWidth,
-        y: Math.random() * screenHeight,
-        speedX: (Math.random() - 0.5) * 4,
-        speedY: (Math.random() - 0.5) * 4,
-      },
-    ]);
-  
-  // 애니메이션 실행 함수
-  const animate = () => {
-    isAnimated.value = true;
-      circles.value.forEach(circle => {
-        // 원의 위치 업데이트
-        circle.x += circle.speedX;
-        circle.y += circle.speedY;
+  {
+    size: 500,
+    x: Math.random() * screenWidth,
+    y: Math.random() * screenHeight,
+    speedX: (Math.random() - 0.5) * 4,
+    speedY: (Math.random() - 0.5) * 4,
+  },
+  {
+    size: 500,
+    x: Math.random() * screenWidth,
+    y: Math.random() * screenHeight,
+    speedX: (Math.random() - 0.5) * 4,
+    speedY: (Math.random() - 0.5) * 4,
+  },
+  {
+    size: 500,
+    x: Math.random() * screenWidth,
+    y: Math.random() * screenHeight,
+    speedX: (Math.random() - 0.5) * 4,
+    speedY: (Math.random() - 0.5) * 4,
+  },
+]);
 
+// 애니메이션 실행 함수
+const animate = () => {
+  isAnimated.value = true;
+  circles.value.forEach((circle) => {
+    // 원의 위치 업데이트
+    circle.x += circle.speedX;
+    circle.y += circle.speedY;
 
-         // 원의 위치가 화면을 벗어나지 않도록 제한
-         circle.x = Math.max(circle.size / 2, Math.min(circle.x, screenWidth - circle.size / 2));
-        circle.y = Math.max(circle.size / 2, Math.min(circle.y, screenHeight - circle.size / 2));
+    // 원의 위치가 화면을 벗어나지 않도록 제한
+    circle.x = Math.max(
+      circle.size / 2,
+      Math.min(circle.x, screenWidth - circle.size / 2)
+    );
+    circle.y = Math.max(
+      circle.size / 2,
+      Math.min(circle.y, screenHeight - circle.size / 2)
+    );
 
-        // 벽에 부딪히면 속도 반전
-        if (circle.x === circle.size / 2 || circle.x === screenWidth - circle.size / 2) {
-          circle.speedX = -circle.speedX;
-        }
-        if (circle.y === circle.size / 2 || circle.y === screenHeight - circle.size / 2) {
-          circle.speedY = -circle.speedY;
-        }
-      });
-      // 요청 애니메이션 프레임을 이용해 반복 실행
-      requestAnimationFrame(animate);
-    };
+    // 벽에 부딪히면 속도 반전
+    if (
+      circle.x === circle.size / 2 ||
+      circle.x === screenWidth - circle.size / 2
+    ) {
+      circle.speedX = -circle.speedX;
+    }
+    if (
+      circle.y === circle.size / 2 ||
+      circle.y === screenHeight - circle.size / 2
+    ) {
+      circle.speedY = -circle.speedY;
+    }
+  });
+  // 요청 애니메이션 프레임을 이용해 반복 실행
+  requestAnimationFrame(animate);
+};
 
-    // 컴포넌트가 마운트될 때 애니메이션 시작
-    onUpdated(() => {
-      if(!isAnimated.value) animate();
-    });
-
+// 컴포넌트가 마운트될 때 애니메이션 시작
+onUpdated(() => {
+  if (!isAnimated.value) animate();
+});
 </script>
 
 <style>
-  /* .user-bg {
+/* .user-bg {
     background: linear-gradient(
     90deg,
     var(--bg-gradient-color1),
@@ -124,11 +148,11 @@ const circles = ref([
     var(--bg-gradient-color3)
   );
   } */
-  .circle {
+.circle {
   position: absolute;
   border-radius: 50%;
   filter: blur(60px);
-  z-index: -1 ;
+  z-index: -1;
 }
 
 #circle0 {
@@ -150,5 +174,4 @@ const circles = ref([
   z-index: 2;
   padding: 30px;
 }
-
 </style>
